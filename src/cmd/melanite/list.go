1c1069a3aa2bb75c4efa1ded6fd5cfb3c49c4412
汪玉斌 <wangyubin@idcos.com>
2016-04-18 22:23:07 +0800

    add skeleton for subcommand list

--- src/cmd/melanite/list.go
+++ src/cmd/melanite/list.go
@@ -1,1 +1,34 @@
 package main
+
+import "github.com/codegangsta/cli"
+
+func initListSubCmd(app *cli.App) {
+	listSubCmd := cli.Command{
+		Name:        "list",
+		Usage:       "list <options>",
+		Description: "list groups and nodes",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "g,group",
+				Value: "",
+				Usage: "list group and it's nodes",
+			},
+		},
+		Action: func(c *cli.Context) {
+			var groupName = c.String("group")
+			if groupName == "" {
+				listGroups()
+			} else {
+				listNodes(groupName)
+			}
+		},
+	}
+}
+
+func listGroups() error {
+	return nil
+}
+
+func listNodes(groupName string) error {
+	return nil
+}

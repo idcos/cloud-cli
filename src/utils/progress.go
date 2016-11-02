90cd21fc2d6f3d2a668678bd969633063407b1b2
汪玉斌 <wangyubin@idcos.com>
2016-11-02 10:08:34 +0800

    change progress length to 100

--- src/utils/progress.go
+++ src/utils/progress.go
@@ -12,8 +12,8 @@ const (
 func PrintFileProgress(filename string, percent int) {
 
 	if percent == 100 {
-		fmt.Printf("%-30s  100%%[%s]\n", strings.Repeat(PercentChar, 30), filename)
+		fmt.Printf("%-100s  100%%[%s]\n", strings.Repeat(PercentChar, 100), filename)
 	} else {
-		fmt.Printf("%-30s  %d%%[%s]\r", strings.Repeat("#", 30*percent/100), percent, filename)
+		fmt.Printf("%-100s  %d%%[%s]\r", strings.Repeat("#", percent), percent, filename)
 	}
 }
